4949ce2d563b1252059067aa09d972713164da47
SongGayoung <xjongamx@kyonggi.ac.kr>
2023-10-04 15:35:36 +0900

    [todo] useReducer 마이그레이션

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useReducer, useRef } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
@@ -24,48 +24,69 @@ const mockDate = [
     createdDate: new Date().getTime(),
   },
 ];
+function reducer(state, action) {
+  if (action.type === "CREATE") {
+    return [...state, action.data];
+  }
+  if (action.type === "UPDATE") {
+    return state.map((it) =>
+      it.id === action.data ? { ...it, isDone: !it.isDone } : it
+    );
+  }
+  if (action.type === "DELETE") {
+    return state.filter((it) => it.id !== action.data);
+  }
+}
 
 function App() {
-  // 1. todolist를 위한 상태관리 useState hook
-  const [todos, setTodos] = useState(mockDate);
-  // 3. id 값을 고유값으로 증가하게 하기 위함
-  const idRef = useRef(3);
-  // 2. 생성되는 데이터 값, mock데이터의 프로퍼티와 동일
-  const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content,
-      createdDate: new Date().getTime(),
-    };
+  const [todos, dispatch] = useReducer(reducer, mockDate);
 
-    // 2-1. setTodos로 todolist의 현재 상태로 만들기
-    // newTodo(새로추가) + 기존에 있던 todos로 배열로 보관
-    setTodos([newTodo, ...todos]);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content,
+        createdDate: new Date().getTime(),
+      },
+    });
+    // setTodos([newTodo, ...todos]);
   };
 
   const onUpdate = (targetId) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === targetId) {
-          return {
-            ...todo,
-            isDone: !todo.isDone,
-          };
-        } else {
-          return todo;
-        }
-      })
-    );
+    dispatch({
+      type: "UPDATE",
+      data: targetId,
+    });
+    // setTodos(
+    //   todos.map((todo) => {
+    //     if (todo.id === targetId) {
+    //       return {
+    //         ...todo,
+    //         isDone: !todo.isDone,
+    //       };
+    //     } else {
+    //       return todo;
+    //     }
+    //   })
+    // );
   };
 
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      data: targetId,
+    });
+
+    // setTodos(todos.filter((todo) => todo.id !== targetId));
   };
   return (
     <div className="App">
       <Header />
-      {/* 4. props로 넘겨주기 */}
+
       <TodoEditor onCreate={onCreate} />
       <TodoList todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
