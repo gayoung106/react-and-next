99374b59fb72ea9c3f57f3230a2591450a97c943
SongGayoung <xjongamx@kyonggi.ac.kr>
2023-10-11 15:46:33 +0900

    next: ssr로 전환

--- next-project/pages/index.js
+++ next-project/pages/index.js
@@ -1,32 +1,20 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Home() {
-  const code = "KOR";
-  const router = useRouter();
+export default function Home({ name }) {
+  console.log("홈인데 실행되나 보자");
 
-  const onClickButton = () => {
-    router.push("/search");
-  };
-  return (
-    <div>
-      HOME PAGE
-      <div>
-        <button onClick={onClickButton}>Search 페이지로 이동</button>
-      </div>
-      <div>
-        <Link href={"/search"}>Search 페이지로 이동</Link>
-      </div>
-      <div>
-        <Link
-          href={{
-            pathname: "/country/[code]",
-            query: { code: code },
-          }}
-        >
-          {code} 페이지로 이동
-        </Link>
-      </div>
-    </div>
-  );
+  useEffect(() => {
+    console.log("그래 브라우저에서만 실행해");
+  }, []);
+  return <div>{name}</div>;
 }
+
+export const getServerSideProps = async () => {
+  // ssr을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
+
+  return {
+    props: {
+      name: "KOREA",
+    },
+  };
+};
