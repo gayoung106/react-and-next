f80f532ede8a8b72acaf8bf528e6d35bccdaff47
SongGayoung <xjongamx@kyonggi.ac.kr>
2023-10-11 16:52:26 +0900

    next: ssr 적용

--- next-project/pages/index.js
+++ next-project/pages/index.js
@@ -1,20 +1,22 @@
-import { useEffect } from "react";
+import { fetchCountries } from "@/api";
 
-export default function Home({ name }) {
-  console.log("홈인데 실행되나 보자");
-
-  useEffect(() => {
-    console.log("그래 브라우저에서만 실행해");
-  }, []);
-  return <div>{name}</div>;
+export default function Home({ countries }) {
+  return (
+    <div>
+      {countries.map((country) => (
+        <div key={country.code}>{country.commonName}</div>
+      ))}
+    </div>
+  );
 }
 
 export const getServerSideProps = async () => {
-  // ssr을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
+  // API 호출 코드가 필요
+  const countries = await fetchCountries();
 
   return {
     props: {
-      name: "KOREA",
+      countries,
     },
   };
 };
